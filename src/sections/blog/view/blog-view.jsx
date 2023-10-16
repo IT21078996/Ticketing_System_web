a212c62b3526c14bf57b0557e7903351fc5fe180
it21104176 <it21104176@my.sliit.lk>
2023-10-16 09:30:26 +0530

    'Updated'

--- src/sections/blog/view/blog-view.jsx
+++ src/sections/blog/view/blog-view.jsx
@@ -1,46 +1,176 @@
+import React, { useState } from 'react';
+
 import Stack from '@mui/material/Stack';
+import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
-
-import { posts } from 'src/_mock/blog';
-
-import Iconify from 'src/components/iconify';
-
-import PostCard from '../post-card';
-import PostSort from '../post-sort';
-import PostSearch from '../post-search';
-
-// ----------------------------------------------------------------------
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
 
 export default function BlogView() {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [route, setRoute] = useState('');
+  const [showPaymentForm, setShowPaymentForm] = useState(false);
+  const [cardHolderName, setCardHolderName] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
+  const [cvv, setCvv] = useState('');
+  const [expirationDate, setExpirationDate] = useState('');
+  const [paymentSuccessDialogOpen, setPaymentSuccessDialogOpen] = useState(false);
+
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    setEndDate(event.target.value);
+  };
+
+  const handleRouteChange = (event) => {
+    setRoute(event.target.value);
+  };
+
+  const handleCardHolderNameChange = (event) => {
+    setCardHolderName(event.target.value);
+  };
+
+  const handleCardNumberChange = (event) => {
+    if (/^\d{0,12}$/.test(event.target.value)) {
+      setCardNumber(event.target.value);
+    }
+  };
+
+  const handleCvvChange = (event) => {
+    if (/^\d{0,3}$/.test(event.target.value)) {
+      setCvv(event.target.value);
+    }
+  };
+
+  const handleExpirationDateChange = (event) => {
+    setExpirationDate(event.target.value);
+  };
+
+  const handlePaymentSuccessDialogClose = () => {
+    setPaymentSuccessDialogOpen(false);
+    // Here, you can navigate to another page if needed
+  };
+
+  const handleSubmit = () => {
+    if (showPaymentForm) {
+      // Handle payment form submission here (you can simulate success with a delay)
+      setTimeout(() => {
+        setPaymentSuccessDialogOpen(true);
+      }, 1000);
+    } else {
+      // Show the payment form
+      setShowPaymentForm(true);
+    }
+  };
+
   return (
     <Container>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h4">Blog</Typography>
+      <Stack direction="column" alignItems="center" mb={5}>
+        <Typography variant="h4">Student Pass</Typography>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <form>
+            <TextField
+              label="Start Date"
+              type="date"
+              value={startDate}
+              onChange={handleStartDateChange}
+              InputLabelProps={{ shrink: true }}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="End Date"
+              type="date"
+              value={endDate}
+              onChange={handleEndDateChange}
+              InputLabelProps={{ shrink: true }}
+              fullWidth
+              margin="normal"
+            />
+            <Select
+              label="Route"
+              value={route}
+              onChange={handleRouteChange}
+              fullWidth
+              margin="normal"
+            >
+              <MenuItem value="Route 1">138 - (Homagama - Pettah)</MenuItem>
+              <MenuItem value="Route 2">138 - (Kottawa - Pettah)</MenuItem>
+              <MenuItem value="Route 3">138 - (Maharagama - Pettah)</MenuItem>
+              <MenuItem value="Route 4">122 - (Awissawella - Pettah)</MenuItem>
+            </Select>
+            <br/><br/>
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+              {showPaymentForm ? 'Next' : 'Next'}
+            </Button>
+            <br/><br/>
+          </form>
 
-        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
-          New Post
-        </Button>
+          {showPaymentForm && (
+            <form>
+              <TextField
+                label="Card Holder Name"
+                value={cardHolderName}
+                onChange={handleCardHolderNameChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                label="Card No"
+                value={cardNumber}
+                onChange={handleCardNumberChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                label="CVV"
+                value={cvv}
+                onChange={handleCvvChange}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                label="Expiration Date"
+                type="date"
+                value={expirationDate}
+                onChange={handleExpirationDateChange}
+                InputLabelProps={{ shrink: true }}
+                fullWidth
+                margin="normal"
+              />
+              <br/><br/>
+            </form>
+          )}
+        </div>
+        {showPaymentForm && (
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        )}
+        <Dialog
+          open={paymentSuccessDialogOpen}
+          onClose={handlePaymentSuccessDialogClose}
+        >
+          <DialogTitle>Payment Success</DialogTitle>
+          <DialogContent>
+            <p>Your payment is done.</p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handlePaymentSuccessDialogClose} color="primary">
+              Get QR Code
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Stack>
-
-      <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
-        <PostSearch posts={posts} />
-        <PostSort
-          options={[
-            { value: 'latest', label: 'Latest' },
-            { value: 'popular', label: 'Popular' },
-            { value: 'oldest', label: 'Oldest' },
-          ]}
-        />
-      </Stack>
-
-      <Grid container spacing={3}>
-        {posts.map((post, index) => (
-          <PostCard key={post.id} post={post} index={index} />
-        ))}
-      </Grid>
     </Container>
   );
 }
